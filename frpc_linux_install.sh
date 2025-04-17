007fea7174b228731a67a24fb203595603eaf983
LoserLiunian <105856566+LoserLiunian@users.noreply.github.com>
2025-04-17 09:00:03 +0800

    Update frpc_linux_install.sh

--- frpc_linux_install.sh
+++ frpc_linux_install.sh
@@ -16,7 +16,7 @@ WORK_PATH=$(dirname $(readlink -f $0))
 FRP_NAME=frpc
 FRP_VERSION=0.61.2
 FRP_PATH=/usr/local/frp
-PROXY_URL="https://ghp.ci/"
+PROXY_URL="https://ghfast.top/"
 
 # check frpc
 if [ -f "/usr/local/frp/${FRP_NAME}" ] || [ -f "/usr/local/frp/${FRP_NAME}.toml" ] || [ -f "/lib/systemd/system/${FRP_NAME}.service" ];then
